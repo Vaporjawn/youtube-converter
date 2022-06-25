0c33fc3449f94ec76a2b5878002b199aa40d13d8
Victor Williams <vrwilliams719@gmail.com>
2022-06-25 01:25:29 -0400

    done

--- src/containers/appContainer.ts
+++ src/containers/appContainer.ts
@@ -1,8 +1,8 @@
-import { ffmpeg } from 'fluent-ffmpeg';
-import { binaries } from 'ffmpeg-static';
+import ffmpeg from 'fluent-ffmpeg';
+import binaries from 'ffmpeg-static';
 
 const appContainer = () => {
-  const convertMp4ToMp3 = (paths) => {
+  const convertMp4ToMp3 = (paths: { filePath: any }) => {
     // Tell the user we are starting to convert the file to mp3.
     // this.setState({ progressMessage: 'Converting...', progress: 0 });
 
@@ -12,14 +12,14 @@ const appContainer = () => {
 
       // Pass ffmpeg the temp mp4 file. Set the path where is ffmpeg binary for the platform. Provided desired format.
       ffmpeg(paths.filePath)
-        .setFfmpegPath(binaries.ffmpegPath())
+        // .setFfmpegPath(binaries.ffmpegPath())
         .format('mp3')
         // .audioBitrate(this.state.bitrate)
         .on('progress', (progress: { percent: number }) => {
           // Use same rate limiting as above in function "getVideoAsMp4()" to prevent UI lag.
           if (!rateLimitTriggered) {
             // this.setState({ progress: Math.floor(progress.percent) });
-            rateLimitstart of working containerTriggered = true;
+            // rateLimitstart of working containerTriggered = true;
             setTimeout(() => {
               rateLimitTriggered = false;
             }, 800);
